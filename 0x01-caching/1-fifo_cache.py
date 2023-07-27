3eed06739737e816a100709306ebf71a90567c74
Alale Tamara-kuro Simon <31554894+Trmrskr@users.noreply.github.com>
2023-07-27 17:46:36 +0100

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,30 +10,25 @@ class FIFOCache(BaseCaching):
 
     def __init__(self):
         """
-            Constructor method
+        Constructor method
         """
         super().__init__()
-        self.__rank = {}
-        self.__rank_key = 1
+        self.order = []
 
     def put(self, key, item):
         """
-            Assign to the dictionary
+        Cache a key-value pair
         """
-
-        if key is not None and item is not None:
-
-            if self.__rank_key > self.MAX_ITEMS:
-                key_to_remove = min(self.__rank.keys())
-                fifo_key = self.__rank.pop(key_to_remove)
-                self.cache_data.pop(fifo_key)
-                self.cache_data.update({key, item})
-                self.__rank.update({self.__rank_key, item})
-            else:
-                self.cache_data.update({key, item})
-                self.__rank.update({self.__rank_key: key})
-
-            self.__rank_key += 1
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """
