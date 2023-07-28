256bf5fb9c75288a2988fcae867c96ac43782bbb
Trmrskr <simonalale@gmail.com>
2023-07-28 09:57:38 -0700

    documentations

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -24,19 +24,28 @@ class LFUCache(BaseCaching):
         if key is not None and item is not None:
             cache_data_len = len(self.cache_data)
             if cache_data_len >= self.MAX_ITEMS:
+                # when max capacity of cache has been exceeded
                 if key in self.__order:
+                    # Simply delete from order
                     x = self.__order.index(key)
                     del self.__order[x]
                 else:
+                    # determine the key to delete before appending
+                    # lst - the least value in frequency
                     lst = min(self.__frequency.values())
+                    # lru - least recent users
                     lru = [k for k, v in self.__frequency.items() if v == lst]
+                    # minimum index of lru
                     lru_idx = min([self.__order.index(k) for k in lru])
+                    # key of minimum index of lru
                     lru_key = self.__order[lru_idx]
+                    # del all occurrence of aforementioned key
                     del self.cache_data[lru_key]
                     print("DISCARD {}".format(lru_key))
                     del self.__order[lru_idx]
                     del self.__frequency[lru_key]
-
+            
+            # Insert new key iterms and begin counting
             self.__order.append(key)
             self.cache_data[key] = item
             if key in self.__frequency:
